eca218d338a64dd1977ef4d2570e12699bac27d2
Thong Nguyen <thongnt.it@gmail.com>
2022-11-17 01:23:53 +0700

    feat: add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp

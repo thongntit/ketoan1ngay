f5d445d4810bf22381c880e567a84fe5e51ae27a
Thong Nguyen <thongnt.it@gmail.com>
2023-06-01 02:21:34 +0700

    feat: revamp NavBar

---
+++ components/NavBar/CompanyLogo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CompanyLogo = () => {
+  return (
+    <div className="text-4xl font-bold tracking-wider">
+      DTN
+      <span className="text-4xl font-bold tracking-wider text-red-600">.</span>
+    </div>
+  )
+}
+
+export default CompanyLogo

7df659089f63b80687884dd540dc48d16e413b8f
Thong Nguyen <thongnt.it@gmail.com>
2023-07-16 21:27:15 +0700

    Release 16-7 (#33)
    * feat: add FAB button (#18)
    
    * Sync main to dev (#21)
    
    * chore: add npmrc to prevent install from company registry (#19)
    
    * feat: add sync from main to dev (#20)
    
    * Feat/merge main to dev (#25)
    
    * chore: add npmrc to prevent install from company registry (#19)
    
    * feat: add sync from main to dev (#20)
    
    * feat: update readme (#22)
    
    * Feat/test sync main to dev (#23)
    
    * feat: update readme
    
    * fix branch name
    
    * sync: main to dev (#24)
    
    * chore: add npmrc to prevent install from company registry (#19)
    
    * feat: add sync from main to dev (#20)
    
    * feat: update readme (#22)
    
    * Feat/test sync main to dev (#23)
    
    * feat: update readme
    
    * fix branch name
    
    * chore: update npmrc to version 16 (#26)
    
    ---------
    
    Co-authored-by: Thong Nguyen <thongnt.it@gmail.com>
    
    * sync: main to dev (#27)
    
    * chore: add npmrc to prevent install from company registry (#19)
    
    * feat: add sync from main to dev (#20)
    
    * feat: update readme (#22)
    
    * Feat/test sync main to dev (#23)
    
    * feat: update readme
    
    * fix branch name
    
    * chore: update npmrc to version 16 (#26)
    
    ---------
    
    Co-authored-by: Thong Nguyen <thongnt.it@gmail.com>
    
    * feat: revamp NavBar (#28)
    
    * chore: add vscode settings to auto format import
    
    * chore: path alias for root folder
    
    * feat: revamp NavBar
    
    * chore(deps): bump vite from 3.2.5 to 3.2.7
    
    Bumps [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite) from 3.2.5 to 3.2.7.
    - [Release notes](https://github.com/vitejs/vite/releases)
    - [Changelog](https://github.com/vitejs/vite/blob/v3.2.7/packages/vite/CHANGELOG.md)
    - [Commits](https://github.com/vitejs/vite/commits/v3.2.7/packages/vite)
    
    ---
    updated-dependencies:
    - dependency-name: vite
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * feat: add config
    
    * feat: add nav level 2
    
    * style: add style for md
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- components/NavBar/Service.tsx
+++ components/NavBar/Service.tsx
@@ -1,16 +1,51 @@
+import { ChevronDownIcon } from '@chakra-ui/icons'
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Portal,
+  useDisclosure,
+} from '@chakra-ui/react'
 import React from 'react'
 
 export type IService = {
   order: string
   title: string
-  childs: any[]
+  id: number
+  parent: number
+  level: '1' | '2' | '3'
 }
 
-const Service = ({ service }: { service: IService }) => {
+const Service = ({
+  service,
+  childServices,
+}: {
+  service: IService
+  childServices: IService[]
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const haveChild = childServices.length > 0
   return (
-    <a className="h-7 cursor-pointer text-lg font-[600] hover:text-red-600">
-      {service.title}
-    </a>
+    <Menu isOpen={isOpen}>
+      <MenuButton
+        className="h-7 cursor-pointer text-lg font-[600] hover:text-red-600"
+        onMouseEnter={onOpen}
+        onMouseLeave={onClose}
+      >
+        {service.title} {haveChild ? <ChevronDownIcon /> : ''}
+      </MenuButton>
+      {haveChild && (
+        <Portal>
+          <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
+            {childServices.map((childService) => (
+              <MenuItem key={childService.id}>{childService.title}</MenuItem>
+            ))}
+          </MenuList>
+        </Portal>
+      )}
+    </Menu>
   )
 }
 

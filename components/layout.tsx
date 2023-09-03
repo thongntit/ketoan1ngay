cea9c2e2893f29809e5edab1759d8fbeb959ef5c
Thong Nguyen <thongnt.it@gmail.com>
2023-09-03 16:15:42 +0700

    feat: move header to layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,15 @@
 import Alert from './alert'
 import FAB from './FAB'
 import Meta from './meta'
+import NavBar from './NavBar'
 
-export default function Layout({ preview, children }) {
+export default function Layout({ preview, children, navSettings }) {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
         {preview && <Alert preview={preview} />}
+        <NavBar items={navSettings} />
         <main>{children}</main>
         <FAB />
       </div>
